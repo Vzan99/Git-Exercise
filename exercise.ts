532e78e51ef5d28b485c070a83325a74ae06d4a5
Vzan99 <prasetyoajinugroho94@gmail.com>
2025-01-23 22:03:32 +0800

    my github exercise

---
+++ exercise.ts
@@ -0,0 +1,15 @@
+// Exercise
+// 1. Write a code to display the multiplication table of a given integer/
+//    Example : Number -> 9
+//    Output  : 9 x 1
+//              9 x 2
+//              ...
+//              9 x 10
+
+let number :number = 9;
+
+for (let i = 1 ; i <= 10 ; i++){
+    console.log(`${number} x ${i}`);
+}
+
+//2. 
